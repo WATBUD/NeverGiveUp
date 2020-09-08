a07f2ee80427145dfae8209750fc539074cf5c98
louischin <z23320785@gmail.com>
2020-09-08 18:33:59 +0800

    測試模組變數無法共用

--- DemoUI/ES6/main.js
+++ DemoUI/ES6/main.js
@@ -1,7 +1,7 @@
 import {aString,aObject} from './lib.js';
+export var TestString="sor";
 export class init{
     at=aString;
-
     constructor() {
         console.log('init',aString,aObject);
         document.getElementById('ShowMessage').innerHTML="TextMassage";
