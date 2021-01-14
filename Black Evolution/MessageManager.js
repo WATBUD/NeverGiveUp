dc487750fa5f0b201fbd5a84d42ef224e33947a9
louischin <z23320785@gmail.com>
2021-01-14 17:32:43 +0800

    新增修改模組

--- Black Evolution/MessageManager.js
+++ Black Evolution/MessageManager.js
@@ -1,17 +1,44 @@
 'use strict';
 
-
-
-
 var Message_UI=document.getElementById('Message_UI');
 
-setInterval(function () {
+// document.removeEventListener("mousedown", undefined);
+// document.removeEventListener("mousedown", 'SSS');
+
+
+document.addEventListener('mousedown',zz);
+//document.removeEventListener("mousedown",zz);
+document.addEventListener('mousedown',AAA);
+
+//document.removeEventListener("mousedown",sss);
+
+document.removeEventListener("mousedown",null);
+function zz(){
+    console.log("FFFF");
     Message_UI.innerHTML+=
     "<div id=\"messageAttack\" class=\"Message_Item\">\
-    您攻擊了 哥布林 造成 &lt;250&gt;傷害\
+    您攻擊了 zz 造成 &lt;250&gt;傷害\
     </div>"
-    //Message_UI.appendChild(createDiv);
-}, 500);
+}
+function AAA(){
+    console.log("FFFF");
+    Message_UI.innerHTML+=
+    "<div id=\"messageAttack\" class=\"Message_Item\">\
+    您攻擊了 AAA 造成 &lt;250&gt;傷害\
+    </div>"
+}
+
+
+
+
+
+// setInterval(function () {
+//     Message_UI.innerHTML+=
+//     "<div id=\"messageAttack\" class=\"Message_Item\">\
+//     您攻擊了 哥布林 造成 &lt;250&gt;傷害\
+//     </div>"
+//     //Message_UI.appendChild(createDiv);
+// }, 500);
 // var createDiv = document.createElement('div');
 // createDiv.classList.add("circle");
 
