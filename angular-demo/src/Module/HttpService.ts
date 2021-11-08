ee94a34e6846c2981a28a24c097cd07a4c8ad0e6
louischin <z23320785@gmail.com>
2021-11-08 17:02:35 +0800

    暫定代理功能

--- angular-demo/src/Module/HttpService.ts
+++ angular-demo/src/Module/HttpService.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -7,13 +8,14 @@ import { Injectable } from '@angular/core';
 
 export class HttpService {
 
-    //"Access-Control-Allow-Origin": "http://localhost:4200",
+    //"Access-Control-Allow-Origin": "http://localhost:4200",'rejectUnauthorized': 'false'
     public httpOptions = {
         headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": "www.adata.com",
             'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
             'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin, Content-Type, Accept, Accept-Language, Origin, User-Agent',
+            "Access-Control-Max-Age": "86400",
+            
         })
     };
 
@@ -25,8 +27,21 @@ export class HttpService {
 
 
     getURL(URL) {
-        //this.http.get<any>(URL, this.httpOptions);  
-        return this.http.get<any>(URL, this.httpOptions);
+        var rstarget=this.http.get(URL, this.httpOptions);
+        console.log('%c rstarget', 'color:rgb(255,75,255,1)', rstarget);
+
+        return rstarget;  
+        //return this.http.get(URL);
     }
+
+    getData(URL) { 
+        this.http.get(URL).subscribe(x=> console.log(x)) ;
+    }
+
+    // public getBackendData(
+    //     URL): Observable<any> {
+    //     //const getURL = URL;
+    //       return this.http.get<any>(URL, this.httpOptions);  
+    // }
 
 }
--- angular-demo/src/app/httprequest/httprequest.component.ts
+++ angular-demo/src/app/httprequest/httprequest.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { HttpService } from '../../Module/HttpService';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-httprequest',
   templateUrl: './httprequest.component.html',
@@ -13,6 +13,7 @@ export class HTTPRequestComponent implements OnInit {
   title = 'Article by Jeetendra';
   posts: any;
   prime_json = "https://www.adata.com/upload/prime/version.json";
+  prime_json2 = "/upload/prime/version.json";
   private url = 'https://my-json-server.typicode.com/JSGund/XHR-Fetch-Request-JavaScript/posts';
 
   constructor(private http: HttpClient, private httpService: HttpService) {
@@ -45,15 +46,40 @@ export class HTTPRequestComponent implements OnInit {
     //   }
     // );
 
-    this.httpService.getURL(this.prime_json).subscribe(
-      (response) => {
-        console.log('%c getURL', 'color:rgb(255,75,255,1)', response);
-        //this.posts = response;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    // this.httpService.getURL(this.prime_json).subscribe(
+    //   (response) => {
+    //     console.log('%c getURL', 'color:rgb(255,75,255,1)', response);
+    //     //this.posts = response;
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
+    var httpOptions = {
+      headers: new HttpHeaders({
+          "Access-Control-Allow-Origin": "www.adata.com",
+          'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin, Content-Type, Accept, Accept-Language, Origin, User-Agent',
+          "Access-Control-Max-Age": "86400",
+          
+      })
+  };
+
+
+    this.http.get(this.prime_json).subscribe(x=> {
+      console.log('%c zzzzzzzzzzz', 'color:rgb(255,75,255,1)', x)
+
+    }) ;
+
+    this.http.get(this.prime_json2).subscribe(x=> {
+      console.log('%c zzzzzzzzzzz', 'color:rgb(255,75,255,1)', x)
+
+    }) ;
+
+
+    
+
+
 
     // var getURL = this.http.get(this.prime_json).subscribe(res => {
     //   // console.log('%c subscribe)getURL', 'color:rgb(255,75,255,1)', getURL);
