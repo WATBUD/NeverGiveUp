d2f6729fa9b4d826636a2874b749a9ba686d72b8
louischin <z23320785@gmail.com>
2021-11-18 19:32:09 +0800

    移除多餘程式

--- angular-demo/src/DemoUI/ES6/main.js
+++
@@ -1,9 +0,0 @@
-import {aString,aObject} from './lib.js';
-export var TestString="sor";
-export class init{
-    at=aString;
-    constructor() {
-        console.log('init',aString,aObject);
-        document.getElementById('ShowMessage').innerHTML="TextMassage";
-    }
-}
--- angular-demo/src/app/ngcolor/color-output.ts
+++
@@ -1,9 +0,0 @@
-import { Hsl } from './shared/color-utility/hsl';
-import { Rgb } from './shared/color-utility/rgb';
-
-export interface ColorOutput {
-    rgb: Rgb;
-    hex: number;
-    hexString: string;
-    hsl: Hsl;
-}
